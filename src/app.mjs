51ad39b5948d63ad23bd38f11df48623cb0ccfb8
saadali <saadali6991550@gmail.com>
2025-12-24 12:23:55 +0500

    multiple route handling

--- src/app.mjs
+++ src/app.mjs
@@ -3,12 +3,15 @@ import express from 'express'
 
 const app = express()
 
-app.get('/user', (req, res) => {
+app.use('/user', (req, res,next) => {
   res.send('Hello World express')
+  next()
+},(req,res)=>{
+res.send('welcome to second')
 })
-app.post('/user', (req, res) => {
-  res.send('Hello World ')
-})
+//app.post('/user', (req, res) => {
+ // res.send('Hello World ')
+//})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
