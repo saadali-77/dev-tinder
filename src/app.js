40d7d77d7dc66b96b27283b27f1bce8d1f0bf293
saadali <saadali6991550@gmail.com>
2025-12-25 22:54:41 +0500

    get all user api

--- src/app.js
+++ src/app.js
@@ -4,6 +4,23 @@ const User=  require('./models/user')
  const connectDB=    require('./config/database')
 const app = express()
 app.use(express.json())
+app.get('/user',async(req,res)=>{
+ // const users= req.body.email
+  try{
+    const users= await User.find({})
+   // const user  = await User.find({email:useremail})
+   if(!users){
+  res.status(404).send('did not found data')
+   }else {
+    res.send(users)
+  }
+ } 
+
+ 
+ catch(err){
+
+ }
+})
 app.post('/signup',async(req,res)=>{
  
    const user= new User(req.body)
