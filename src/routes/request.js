64f31b3fcc9fdb169eaaa96a7ba70a4761f70171
saadali <saadali6991550@gmail.com>
2025-12-31 14:29:09 +0500

    send fromid to userid

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,8 +1,24 @@
 const express= require('express')
 const requestRouter= express.Router();
+const ConnectionReq= require('../models/connectionReq')
 const {Auth} =require('../middleware/auth')
-requestRouter.post('/sendconnection',Auth,(req,res)=>{
-  const user= req.user
+requestRouter.post('/request/send/:status/:touserid',Auth,async(req,res)=>{
+  try{
+    const fromuserid= req.user._id;
+  const touserid= req.params.touserid;
+  const status= req.params.status;
+ const connectionReq= new ConnectionReq({
+    touserid,fromuserid,status
+  })
+ const data=  await connectionReq.save()
+  
+  res.json({
+    message:'data sent sucessfully',data
+  })
+}
+  catch(err){
+    res.send('errors + err.message')
+  }
   
   res.send('data sent by'+ user.firstName )
 })
